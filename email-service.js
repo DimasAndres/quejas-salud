161178d2673fec688d21333f28cc1b2478651dc7
DimasAndres <docenteandres@gmail.com>
2025-06-24 22:38:16 -0500

    Update email-service.js

--- email-service.js
+++ email-service.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 // Configuración de destinatarios por departamento (basado en destinatarios_departamento.py)
 const DESTINATARIOS_POR_DEPARTAMENTO = {
     Amazonas: {
-        principal: "servicioalcliente@fiduprevisora.com.co",
-        copia: ["correointernosns@supersalud.gov.co"],
+        principal: "ingenierodimas@gmail.com",
+        copia: [""],
         responsable: "",
     },
     Antioquia: {
